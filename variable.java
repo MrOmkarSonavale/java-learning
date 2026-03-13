0ab09030211b3b342a810547eb70677207c7cf2c
MrOmkarSonavale <mr.sonavaleomkar@gmail.com>
2026-03-13 14:03:59 +0530

    data types in java

--- variable.java
+++ variable.java
@@ -1,5 +1,21 @@
 class Variable {
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        byte b = 5;
+        short s = 10;
+        int i = 4000;
+        long l = 10000;
+
+        float f = 14.24f;
+        double d = 23.34353;
+
+        char c = 'a';
+
+        System.out.println("integer values" + b + " ," + s + " , " + i + " , " + l);
+
+        System.out.println("floating point values" + f + " , " + d);
+
+        System.out.println("Character values" + c);
     }
 }
